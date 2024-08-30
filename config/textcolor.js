1ad05bc5912939ce1dcc6008130fe56543ead1d3
Captain <71719286+CaptainTsu@users.noreply.github.com>
2024-08-30 17:53:39 +0530

    Added textcolor config

---
+++ config/textcolor.js
@@ -0,0 +1,34 @@
+// Don't change anything here if you don't know what you're doing.
+
+module.exports = {
+    // Define foreground colors
+    colors: {
+        white: '[2;37m',
+        teal: '[2;36m',
+        pink: '[2;35m',
+        light_blue: '[2;34m',
+        gold: '[2;33m',
+        yellowish_green: '[2;32m',
+        red: '[2;31m',
+        dark_gray: '[2;30m',
+    },
+    
+    // Define background colors
+    background_colors: {
+        cream_white: '[2;47m',
+        light_gray60: '[2;46m',
+        blurple: '[2;45m',
+        light_gray55: '[2;44m',
+        gray45: '[2;43m',
+        gray40: '[2;42m',
+        rust_brown: '[2;41m',
+        blueish_black: '[2;40m',
+    },
+
+    // Define text styles
+    styles: {
+        bold: '[1;2m',
+        underline: '[4;2m',
+        reset: '[0m'
+    }
+};
